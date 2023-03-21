e3798ce51255b760f779b377fcddf1e2c4aa91f5
khoirulmustofa21 <datatoblenk@gmail.com>
2023-03-21 22:57:59 +0700

    update

--- lib/module/online_class/flutter_book/elogin1/view/elogin1_view.dart
+++ lib/module/online_class/flutter_book/elogin1/view/elogin1_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/core.dart';
 import 'package:hyper_ui/module/online_class/flutter_book/elogin1/view/elogin1_content.dart';
-
 import '../controller/elogin1_controller.dart';
-import 'customColors.dart';
 
 class ELogin1View extends StatefulWidget {
   const ELogin1View({Key? key}) : super(key: key);
@@ -12,7 +10,6 @@ class ELogin1View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
-      backgroundColor: CustomColors.colorBackground,
       appBar: AppBar(
         title: const Text("ELogin1"),
         actions: const [],
